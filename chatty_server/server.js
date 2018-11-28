606011d9c8672a2e8768d841b450e5640e772c61
Joseph Pham <j.pham017@gmail.com>
2018-11-28 16:06:57 -0500

    added server folder

---
+++ chatty_server/server.js
@@ -0,0 +1,157 @@
+// server.js
+
+const express = require('express');
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+const uuidv1 = require('uuid/v1');
+
+// Set the port to 3001
+const PORT = 3001;
+
+// Create a new express server
+const server = express()
+   // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Create the WebSockets server
+const wss = new SocketServer({ server });
+let usersConnected = [];
+const colours = ['red', 'green', 'blue', 'pink'];
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+
+  getImageUrl = (entry) =>{
+  const imageExpression = /^.*(.jpg|.png|.gif)$/gi
+  if(imageExpression.test(entry)){
+    let splitArray = entry.split(' ');
+    if(splitArray.length > 1){
+      const url = splitArray.filter(value => imageExpression.test(value));
+      return url;
+    }
+    //handles entry when only URL is passed
+    return entry;
+  }
+  else{
+    //if no URL is passed imageURL is blank
+    return '';
+  }
+}
+
+  getContent = (entry) => {
+  const imageExpression = /^.*(.jpg|.png|.gif)$/gi
+    let splitArray = entry.split(' ');
+  if(imageExpression.test(entry)){
+
+    if(splitArray.length > 1){
+      const msg = splitArray.filter(value => (!imageExpression.test(value)));
+      return msg;
+    }
+    else{
+      return '';
+    }
+  }
+    return entry;
+  }
+
+
+
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  usersConnected.push(ws);
+  const random = colours[Math.floor(Math.random()*colours.length)];
+  let users = {
+    type : "incomingUsers",
+    numberOfUsers : usersConnected.length,
+    colours : random
+  }
+
+  wss.clients.forEach(function each(client){
+    if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(users));
+    }
+  })
+
+  ws.on('message', function(data) {
+    let uid = JSON.parse(data);
+    //let image = JSON.parse(data);
+
+    switch(uid.type) {
+      case 'postMessage':
+        uid.id = uuidv1();
+        uid.type = "incomingMessage";
+        uid.imageURL = getImageUrl(uid.content);
+        uid.content = getContent(uid.content);
+        uid.colours = random;
+      break;
+
+      case 'postNotification' :
+        uid.type = "incomingNotification";
+      break;
+    }
+    data = JSON.stringify(uid);
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+
+
+      // if(uid.type === 'postMessage'){
+      //   uid.id = uuidv1();
+      //   uid.type = "incomingMessage";
+      //   data = JSON.stringify(uid);
+      //   //console.log(data)
+      //   wss.clients.forEach(function each(client) {
+      //     //console.log(client !== ws && client.readyState === WebSocket.OPEN);
+      //     if (/*client !== ws && */client.readyState === WebSocket.OPEN) {
+      //       client.send(data);
+      //     }
+      //   });
+      // }
+
+      // if(uid.type === 'postNotification'){
+      //   uid.type = "incomingNotification";
+      //   data = JSON.stringify(uid);
+      //   console.log(data)
+      //   wss.clients.forEach(function each(client) {
+      //     //console.log(client !== ws && client.readyState === WebSocket.OPEN);
+      //     if (/*client !== ws && */client.readyState === WebSocket.OPEN) {
+      //       client.send(data);
+      //     }
+      //   });
+      // }
+
+      //  if(uid.type === 'postUsers'){
+      //   uid.type = "incomingUsers";
+      //   data = JSON.stringify(uid);
+      //   console.log(data)
+      //   wss.clients.forEach(function each(client) {
+      //     //console.log(client !== ws && client.readyState === WebSocket.OPEN);
+      //     if (/*client !== ws && */client.readyState === WebSocket.OPEN) {
+      //       client.send(data);
+      //     }
+      //   });
+      // }
+  });
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    usersConnected.pop();
+    let users = {
+      type : "incomingUsers",
+      numberOfUsers : usersConnected.length
+    }
+    wss.clients.forEach(function each(client){
+      if (/*client !== ws && */client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify(users));
+      }
+    })
+    });
+});
+
